a278a6220be9bf2360ce457ec6ab0bb1f1d23d8e
Hoang Vu Nguyen <vu.nh79@yahoo.com>
2018-07-16 11:21:28 +0700

    part 1

--- savjeecoin/main.js
+++ savjeecoin/main.js
@@ -36,6 +36,26 @@ class Blockchain{
         this.chain.push(newBlock);
 
     }
+
+    // 
+    isChainValid(){
+        for(let i = 1; i< this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            // 
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+            // 
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+
+        return true;
+       
+    }
 }
 
 // 
@@ -46,3 +66,11 @@ savjeeCoin.addBlock(new Block(2, "07/16/2018", { amount: 12}));
 
 // 
 console.log(JSON.stringify(savjeeCoin, null, 4));
+console.log('Is blockchain valid? ' + savjeeCoin.isChainValid());
+
+savjeeCoin.chain[1].data = { amount: 4 };
+savjeeCoin.chain[1].hash = savjeeCoin.chain[1].calculateHash();
+
+console.log(JSON.stringify(savjeeCoin, null, 4));
+
+console.log('Is blockchain valid? ' + savjeeCoin.isChainValid());
